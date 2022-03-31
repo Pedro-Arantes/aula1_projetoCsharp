e78864d03d7beced21864c968a0b29e977fc47b3
Pedro Vianna <pedroviannabiomed@gmail.com>
2022-03-31 10:57:33 -0300

    Projeto C# 1 aula

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projetoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
